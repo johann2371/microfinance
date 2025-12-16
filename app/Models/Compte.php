9ddad02c218cbee5f805b67ad3c41eb6fabf4c5b
Gouaffo johann <jgouaffo@gmail.com>
2025-12-16 08:59:45 +0100

    ajout des entités remplir

---
+++ app/Models/Compte.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Compte extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['client_id', 'type_compte', 'solde'];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
---
+++ app/Models/Transaction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['compte_id', 'type', 'montant', 'description'];
+
+    public function compte()
+    {
+        return $this->belongsTo(Compte::class);
+    }
+}
+s
